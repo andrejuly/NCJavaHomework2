b7e02caddec2aef384418252be675ab8ae984792
Andrey Ulyanov <andrejuly@yandex.ru>
2021-03-29 13:20:03 +0300

    Fix Lottery.java

--- src/com/ulyanovcompany/chapter1/task13/Lottery.java
+++ src/com/ulyanovcompany/chapter1/task13/Lottery.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 
 public class Lottery {
 
-   private static void addArray(ArrayList arrayList) {
-       for (int i = 1; i < 50; i++) {
-           arrayList.add(i);
-       }
-   }
+    private static void addArray(ArrayList arrayList) {
+        for (int i = 1; i < 50; i++) {
+            arrayList.add(i);
+        }
+    }
 
+    private static int random() {
+        return (int) Math.random() * 49 + 1;
+    }
 
-   private static int random () {
-       return (int) Math.random() * 49 + 1;
-   }
-
-   public static ArrayList lottery() {
-       ArrayList arrayList = new ArrayList();
-       addArray(arrayList);
-       for (int i = 0; i <= 6; i++) {
-           arrayList.remove((int) (Math.random() * 49) + 1);
-       }
-       return arrayList;
-   }
+    public static int[] lottery() {
+        ArrayList<Integer> arrayList = new ArrayList();
+        int[] ticket = new int[6];
+        addArray(arrayList);
+        for (int i = 0; i < 6; i++) {
+            int index = (int) (Math.random() * 49) + 1;
+            ticket[i] = arrayList.get(index);
+            arrayList.remove(index);
+        }
+        return ticket;
+    }
 
 }
--- src/com/ulyanovcompany/chapter1/task13/Test.java
+++ src/com/ulyanovcompany/chapter1/task13/Test.java
@@ -1,12 +1,10 @@
 package com.ulyanovcompany.chapter1.task13;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList <Integer> arrayList = new ArrayList(Lottery.lottery());
-        for (Integer a: arrayList) {
-            System.out.println(a);
-        }
+        int[] test = Lottery.lottery();
+        Arrays.stream(test).sorted().forEach(System.out::println);
     }
 }
